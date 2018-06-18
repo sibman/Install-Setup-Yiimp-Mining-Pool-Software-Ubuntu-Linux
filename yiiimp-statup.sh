e673a789bb9ee7c5b0ea91a2b80c7a5ca8493415
Yiimp mining pool configuration and coins daemon installation <goodthebest123@yahoo.com>
2018-06-18 05:37:19 +0400

    Start yiimp needed screen sessions

--- yiiimp-statup.sh
+++ yiiimp-statup.sh
@@ -3,4 +3,5 @@ sudo screen -dmS main $WEB_DIR/main.sh
 sudo screen -dmS loop2 $WEB_DIR/loop2.sh
 sudo screen -dmS blocks $WEB_DIR/blocks.sh
 # Stratum instances
+
 #sudo screen -dmS [algoname] $STRATUM_DIR/run.sh [algoname]
